faa99e5414a7e33e5fed3aa5d2392fa77878b0a6
Scott Lambert <s-lambert@live.com.au>
2023-02-18 10:53:59 +0800

    Stable car floating

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,8 @@ use bevy_rapier3d::prelude::*;
 
 const WINDOW_SIZE: f32 = 500.0;
 
-#[derive(Component, Deref, DerefMut)]
-struct WheelPoints(Vec<Vec3>);
+#[derive(Component)]
+struct WheelPoints;
 
 fn setup_scene_camera(mut commands: Commands) {
     commands.spawn(Camera3dBundle {
@@ -36,46 +36,84 @@ fn setup_scene(
             transform: Transform::from_xyz(0.0, -0.1, 0.0),
             ..default()
         },
+        RigidBody::Fixed,
         Collider::cuboid(10.0, 0.1, 10.0),
     ));
 
-    commands.spawn((
-        MaterialMeshBundle {
-            mesh: meshes.add(Mesh::from(shape::Box::new(1.6, 1.0, 3.0))),
-            material: materials.add(Color::AZURE.into()),
-            transform: Transform::from_xyz(0.0, 0.5, 0.0),
-            ..default()
-        },
-        RigidBody::Dynamic,
-        Collider::cuboid(0.8, 0.5, 1.5),
-        ExternalForce::default(),
-        WheelPoints(vec![
-            // Front left
-            Vec3::new(0.8, -0.5, 1.5),
-            // Front right
-            Vec3::new(-0.8, -0.5, 1.5),
-            // Back left
-            Vec3::new(0.8, -0.5, -1.5),
-            // Back right
-            Vec3::new(-0.8, -0.5, -1.5),
-        ]),
-    ));
+    commands
+        .spawn((
+            MaterialMeshBundle {
+                mesh: meshes.add(Mesh::from(shape::Box::new(1.6, 1.0, 3.0))),
+                material: materials.add(Color::AZURE.into()),
+                transform: Transform::from_xyz(0.0, 2.0, 0.0),
+                ..default()
+            },
+            RigidBody::Dynamic,
+            Collider::cuboid(0.8, 0.5, 1.5),
+            ExternalForce::default(),
+            // This damping is to stabilise the wheel forces, might need to be implemented when calculating the force
+            Damping {
+                linear_damping: 5.0,
+                angular_damping: 1.0,
+            },
+        ))
+        .with_children(|parent| {
+            let wheel_points = vec![
+                // Front left
+                Vec3::new(0.8, -0.5, 1.5),
+                // Front right
+                Vec3::new(-0.8, -0.5, 1.5),
+                // Back left
+                Vec3::new(0.8, -0.5, -1.5),
+                // Back right
+                Vec3::new(-0.8, -0.5, -1.5),
+            ];
+
+            for wheel_point in wheel_points {
+                parent.spawn((
+                    WheelPoints,
+                    TransformBundle::from_transform(Transform::from_translation(wheel_point)),
+                ));
+            }
+        });
 }
 
 fn bump_character(
-    keyboard_input: Res<Input<KeyCode>>,
-    mut query: Query<(&mut ExternalForce, &WheelPoints)>,
+    rapier_context: Res<RapierContext>,
+    mut query: Query<&mut ExternalForce, Without<WheelPoints>>,
+    wheel_points_query: Query<&GlobalTransform, With<WheelPoints>>,
 ) {
-    let Some((mut forces, wheel_points)) = query.iter_mut().next() else { return };
+    let Some(mut forces) = query.iter_mut().next() else { return };
 
-    if keyboard_input.pressed(KeyCode::Up) {
-        let force = Vec3::new(0.0, 100.0, 0.0);
-        let torque: Vec3 = wheel_points.iter().map(|point| point.cross(force)).sum();
-        forces.force = force;
-        forces.torque = torque;
-    } else {
-        *forces = ExternalForce::default();
+    let force = Vec3::new(0.0, 18.0, 0.0);
+
+    let mut total_force = Vec3::default();
+    let mut total_torque: Vec3 = Vec3::default();
+
+    let ray_dir = Vec3::NEG_Y;
+    let max_toi = 1.0;
+    let solid = true;
+    let query_filter = QueryFilter::only_fixed();
+
+    for wheel_point in wheel_points_query.iter() {
+        if let Some((_entity, toi)) = rapier_context.cast_ray(
+            wheel_point.translation(),
+            ray_dir,
+            max_toi,
+            solid,
+            query_filter,
+        ) {
+            let strength = 1.0 - toi / max_toi;
+            dbg!(strength);
+            let wheel_force = force * strength;
+            let wheel_torque = wheel_point.translation().cross(wheel_force) * strength;
+            total_torque += wheel_torque;
+            total_force += wheel_force;
+        }
     }
+
+    forces.force = total_force;
+    forces.torque = total_torque;
 }
 
 fn main() {
